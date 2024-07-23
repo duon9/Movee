34b5f95d929b7941aab48329cbd3a65399798b8e
duong <ngducduong305@gmail.com>
2024-07-24 00:06:02 +0700

    first commit

---
+++ members/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def members(request):
+    return HttpResponse("Hello world")
+
+def home(request):
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render())
